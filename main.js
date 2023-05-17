3c8115a5d83a64f55a47c3d9795bb5e146d78f53
Sentino <133702488+SENTIN0@users.noreply.github.com>
2023-05-17 13:53:49 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000; // Use the PORT environment variable, or default to 3000
+
+app.get('/status', (req, res) => {
+
+  const response = {
+
+    status: 'ok',
+
+    tool: 'enabled',
+
+    
+
+    version: 'one'
+
+  };
+
+  res.json(response);
+
+});
+
+app.listen(port, () => {
+
+  console.log(`Server listening on port ${port}`);
+
+});
+
